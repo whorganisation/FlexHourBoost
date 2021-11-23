c1e905d921736676b031dc6b77b8ef3a340db6f4
whorganisation21 <whorganisation21@gmail.com>
2021-11-23 03:13:19 +0000

    Test 2

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on("loggedOn", () => {
     console.log(config.username,"You has been logged succesfully.");
 
     client.setPersona(SteamUser.EPersonaState.Online);
-    client.gamesPlayed(["Custom Game Title",730,440]);  
+    client.gamesPlayed(["XRT",730,440]);  
     // 730 is ID of the Game on the Steam
     // Ex 730 = CS:GO
     // You can iddle up to 32 games
